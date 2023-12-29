7fc85e072c89bf38420c6ccddef07338acd0c1a1
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-12-29 23:05:08 +0600

    All filter done and responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,48 @@ import '../App.css'
 
 const Home = () => {
 
-const {
-  state: { products },
-  
-} = CartState();
+  const {
+    state: { products },
+    productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
+  } = CartState();
 
-console.log(products);
+  const transformProducts = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
    
   return (
     <div className="home">
       <Filter />
       <div className="productContainer">
-        {products.map((prod) => (
+        {transformProducts().map((prod) => (
           <SingleProduct prod={prod} key={prod.id} />
         ))}
       </div>

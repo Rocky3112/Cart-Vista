488fd3a59059c650d75f4275b85d5c3ae668eba4
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-12-29 21:52:09 +0600

    added to cart and delete from there successfull

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,96 @@
-import React from 'react'
-import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap'
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import { CartState } from '../context/Context';
-
+import { AiFillDelete } from "react-icons/ai";
+import {
+  Badge,
+  Button,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import { CartState } from "../context/Context";
+import "../App.css";
 
 const Header = () => {
   const {
     state: { cart },
+    dispatch,
+    productDispatch,
   } = CartState();
 
-  console.log(cart);
   return (
-  <Navbar bg='dark' variant='dark' style={{height:80}}>
-    <Container>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
         <Navbar.Brand>
-            <Link href="/">Cart Vista</Link>
+          <Link to="/">Shopping Cart</Link>
         </Navbar.Brand>
-        <Navbar.Text className='search'>
-            <FormControl style={{width:500}} placeholder='Search a produch' className='m-auto'>
-
-            </FormControl>
-        </Navbar.Text>
-
+        {useLocation().pathname.split("/")[1] !== "cart" && (
+          <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              type="search"
+              placeholder="Search a product..."
+              className="m-auto"
+              aria-label="Search"
+              onChange={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_SEARCH",
+                  payload: e.target.value,
+                });
+              }}
+            />
+          </Navbar.Text>
+        )}
         <Nav>
-           <Dropdown  alignright ='true'>
-           <Dropdown.Toggle variant='success'>
-           <FaShoppingCart  color='white' fontSize="25px"/>
-           <Badge> {10}</Badge>
-            </Dropdown.Toggle> 
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{cart.length}</Badge>
+            </Dropdown.Toggle>
 
-            <Dropdown.Menu style={{minWidth:370}}>
-                <span style={{padding:10}}>cart is empty!</span>
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
             </Dropdown.Menu>
-           </Dropdown>
+          </Dropdown>
         </Nav>
-    </Container>
-  </Navbar>
-)}
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
